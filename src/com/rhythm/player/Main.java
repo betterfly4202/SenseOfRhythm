c60271aab3725e040401978efb407b6ae74b5612
이충일 <betterfly4202@gmail.com>
2018-11-07 00:32:22 +0900

    start project

---
+++ src/com/rhythm/player/Main.java
@@ -0,0 +1,10 @@
+package com.rhythm.player;
+
+/**
+ * Created by betterFLY on 07/11/2018.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/rhythm/player/SenseOfRhythm.java
@@ -0,0 +1,13 @@
+package com.rhythm.player;
+
+import javax.swing.*;
+
+/**
+ * Created by betterFLY on 07/11/2018.
+ */
+public class SenseOfRhythm extends JFrame {
+
+    public SenseOfRhythm(){
+        setTitle("Sense Of Rhythm");
+    }
+}
