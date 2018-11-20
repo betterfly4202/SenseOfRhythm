59a4e60ac845b30f91efd83a5a047229b11f1076
betterfly88 <betterfly88@gmail.com>
2018-11-21 01:04:18 +0900

    part 1

--- src/main/java/app/rhythm/player/Main.java
+++ src/main/java/app/rhythm/player/Main.java
@@ -1,13 +1,9 @@
 package app.rhythm.player;
 
-import javazoom.jl.player.Player;
-
-import java.io.File;
-import java.io.FileInputStream;
-
 /**
  * Created by betterFLY on 07/11/2018.
  */
+
 public class Main {
     public static final int SCREEN_WIDTH = 1280;
     public static final int SCREEN_HEIGHT = 720;
--- src/main/java/app/rhythm/player/SenseOfRhythm.java
+++ src/main/java/app/rhythm/player/SenseOfRhythm.java
@@ -2,6 +2,9 @@ package app.rhythm.player;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 
 /**
  * Created by betterFLY on 07/11/2018.
@@ -14,7 +17,14 @@ public class SenseOfRhythm extends JFrame {
 
     private Image introBackground = new ImageIcon(this.getClass().getResource("/images/background/intro.jpg")).getImage();
     private JLabel menuBar = new JLabel(new ImageIcon(this.getClass().getResource("/images/interface/menuBar.png")));
-    private JButton exitButton = new JButton(new ImageIcon(this.getClass().getResource("/images/button/btn_exit_basic.png")));
+
+    //FIXME : IMAGE화면 자체를 필요로함
+    private ImageIcon exitButtonEnteredImage = new ImageIcon(this.getClass().getResource("/images/button/btn_exit_entered.png"));
+    private ImageIcon exitButtonBasicImage =new ImageIcon(this.getClass().getResource("/images/button/btn_exit_basic.png"));
+
+    private JButton exitButton = new JButton(exitButtonBasicImage);
+
+    private int mouseX,mouseY;
 
     public SenseOfRhythm(){
         setUndecorated(true); // -> GUI 기본 메뉴바 제거
@@ -24,17 +34,65 @@ public class SenseOfRhythm extends JFrame {
         setLocationRelativeTo(null); // 화면의 정 중앙
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 프로그램 종료 후 실제로 프로세스를 종료시킴
         setVisible(true); // 실제로 인터페이스를 표출
-        setBackground(new Color(110, 110, 100, 0)); // paintComponents(g) 호출 시 배경을 흰색으로
         setLayout(null); // btn, jlabel 삽입시 위치에 맞도록 설정
+        setBackground(new Color(0, 0, 0, 0)); // paintComponents(g) 호출 시 배경을 흰색으로
 
+        insertExitButton();
+        insertMenuBar();
+
+//        Music music = new Music("intro_music.mp3", true);
+//        music.start();
+    }
+
+    public void insertMenuBar(){
         menuBar.setBounds(0, 0, 1280, 30);
+
+        menuBar.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                mouseX = e.getX();
+                mouseY = e.getY();
+            }
+        });
+
+        menuBar.addMouseMotionListener(new MouseMotionAdapter() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                int x = e.getXOnScreen();
+                int y = e.getYOnScreen();
+                // x,y좌표 갖고와서 JFrame 인터페이스 위치 이동
+                setLocation(x, y);
+            }
+        });
+
         add(menuBar);
+    }
 
-        exitButton.setBounds(50, 50, 30, 30);
+    public void insertExitButton(){
+        exitButton.setBounds(1245, 0, 30, 30);
+        exitButton.setBorderPainted(false);
+        exitButton.setContentAreaFilled(false);
+        exitButton.setFocusPainted(false);
+
+        exitButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                exitButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+                exitButton.setIcon(exitButtonEnteredImage);
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e){
+                exitButton.setIcon(exitButtonBasicImage);
+                exitButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+            }
+            @Override
+            public void mousePressed(MouseEvent e){
+                System.exit(0);
+            }
+        });
+
         add(exitButton);
-
-        Music music = new Music("intro_music.mp3", true);
-        music.start();
     }
 
     public void paint(Graphics g){
@@ -47,7 +105,6 @@ public class SenseOfRhythm extends JFrame {
     public void screenDraw(Graphics g) {
         g.drawImage(introBackground, 0, 0, null);
         paintComponents(g);
-//        this.revalidate();
-        repaint();
+        this.repaint();
     }
 }
